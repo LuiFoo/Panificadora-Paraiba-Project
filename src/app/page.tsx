8fc69fd42a37b26498c4b15c52752ed1fe120a69
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-03-04 23:04:12 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import DocesPage from "@/components/produtos/doces";
 import PaesDocesPage from "@/components/produtos/paes-doces";
 import PaesSalgadosPage from "@/components/produtos/paes-salgados";
 import SalgadosPage from "@/components/produtos/salgados";
-import Image from "next/image";
 
 export default function Home() {
   return (
