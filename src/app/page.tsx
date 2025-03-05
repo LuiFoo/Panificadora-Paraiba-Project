63d9f72ce50670698e1177277b29411ae5ae1c75
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-03-05 00:14:38 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
 import Cardapio from "@/components/cardapio";
-import BebidasPage from "@/components/produtos/bebidas";
-import DocesPage from "@/components/produtos/doces";
-import PaesDocesPage from "@/components/produtos/paes-doces";
-import PaesSalgadosPage from "@/components/produtos/paes-salgados";
-import SalgadosPage from "@/components/produtos/salgados";
 
 export default function Home() {
   return (
