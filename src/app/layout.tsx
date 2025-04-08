7a34a96b4fb835ec5f3e5c2d5f79f291afbb32bf
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-04-08 16:51:37 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,14 +24,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-
-
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Aboreto&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap" rel="stylesheet" />
-      </head>
-
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
