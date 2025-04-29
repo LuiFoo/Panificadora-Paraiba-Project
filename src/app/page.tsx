af51503ce8218e2bba55f5e6312bc6574cd0b263
Luiz Guilherme Rótulo <luizgrbt@gmail.com>
2025-04-28 21:40:11 -0300

    ATT

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import MainPrincipal from "@/components/mains/Principal";
+import MainPrincipal from "@/components/Mains/Principal";
 
 export default function Home() {
   return (
